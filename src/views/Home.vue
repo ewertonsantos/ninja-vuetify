a8be357cb940f0f44308314472efc7f8d9e20091
ewertonsantos <ewertonredsnop@gmail.com>
2020-12-20 20:40:03 -0300

    Iniciando

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>THIS IS HOME PAGE</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus corporis a id explicabo dignissimos tempora in error nobis tenetur perspiciatis quae eligendi amet autem sequi, fugit quos illum quo optio.</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
