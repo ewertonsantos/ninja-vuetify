2977a01b0db1626c8e315b551a72672ea54dcfb4
ewertonsantos <ewertonredsnop@gmail.com>
2020-12-22 10:00:51 -0300

    tool tip, sort e cards

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,31 @@
 <template>
   <v-container>
 
+    <v-layout row class="mb-3" >
+
+      <v-tooltip top>
+          <template v-slot:activator="{ on }">
+            <v-btn small text color="grey" @click="sortBy('title')" v-on="on">
+              <v-icon left small>mdi-folder</v-icon>
+              <span right class="caption text-lowercase">by project name</span>
+            </v-btn>
+          </template>
+          <span class="caption text-lowercase">Sorts projects by name</span>
+      </v-tooltip> 
+      
+      <v-tooltip top>
+          <template v-slot:activator="{ on }">
+            <v-btn small text color="grey" @click="sortBy('person') " v-on="on">
+              <v-icon left small >mdi-account</v-icon>
+              <span class="caption text-lowercase">Project by person</span>
+            </v-btn>
+          </template>
+          <span class="caption text-lowercase">Sorts projects by person</span>
+      </v-tooltip> 
+
+    </v-layout>
+    
+
     <v-card flat v-for="project in projects" :key="project.title">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
 
@@ -43,6 +68,11 @@ export default {
         { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
       ]
+   }
+ },
+ methods:{
+   sortBy(prop){
+     this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
    }
  }
 }
